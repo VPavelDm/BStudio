4a9a8e8981969796ee69cda2db33b39d4e700a85
Pavel Vaitsikhouski <pavel.vaitsikhouski@adevinta.com>
2021-09-16 18:15:29 +0300

    Initial Commit

---
+++ bStudio/bStudioApp.swift
@@ -0,0 +1,17 @@
+//
+//  bStudioApp.swift
+//  bStudio
+//
+//  Created by Pavel Vaitsikhouski on 16.09.21.
+//
+
+import SwiftUI
+
+@main
+struct bStudioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
